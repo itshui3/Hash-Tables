26c3beaf53a5434ead5d9ad5ac1a34dd1b5e3d10
Hui <jeffrey.x.hui@gmail.com>
2020-05-07 15:15:41 -0700

    word count

---
+++ applications/word_count/testword.py
@@ -0,0 +1,3 @@
+print('a\ra\na\ta'.replace('\r', ' ').replace('\n', ' ').replace('\t', ' '))
+
+# .replace('\t', ' ').
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,26 @@
-def word_count(s):
+def word_count(s, cache=None):
     # Implement me.
+    cache = {}
+
+    s_arr = s.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ').split(' ')
+
+    print(s_arr)
+    x_arr = []
+    for i in s_arr:
+        i.replace('\t', '')
+    
+    sanitized = map(lambda i : i.strip('":;,.-+=/\\|[]{}()*^&').lower(), s_arr)
+
+    for i in sanitized:
+        if i == "":
+            continue
+        if i not in cache:
+            cache[i] = 1
+        else:
+            cache[i] += 1
+
+    print('cache return', cache)
+    return cache
 
 
 if __name__ == "__main__":
